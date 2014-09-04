4e4c94527057e05c8343562e7d459c196e7c04af
gurimmer <s.reianai@gmail.com>
2014-09-04 22:00:28 +0900

    scaffoldで生成されたORMベースのコントローラをmongoベースに修正

--- fuel/app/classes/controller/task.php
+++ fuel/app/classes/controller/task.php
@@ -3,7 +3,7 @@ class Controller_Task extends Controller_Template{
 
 	public function action_index()
 	{
-        $mongodb = \Mongo_Db::instance();
+        $mongodb = Mongo_Db::instance();
         $data['tasks'] = $mongodb->get('tasks');
 		$this->template->title = "Tasks";
 		$this->template->content = View::forge('task/index', $data);
@@ -14,8 +14,8 @@ class Controller_Task extends Controller_Template{
 	{
 		is_null($id) and Response::redirect('task');
 
-        $mongodb = \Mongo_Db::instance();
-        if ( ! $data['task'] = $mongodb->get_where('task', array('id' => $id)))
+        $mongodb = Mongo_Db::instance();
+        if ( ! $data['task'] = $mongodb->where(array('id' => $id))->get('tasks'))
         {
             Session::set_flash('error', 'Could not find task #'.$id);
             Response::redirect('task');
@@ -34,8 +34,15 @@ class Controller_Task extends Controller_Template{
 			
 			if ($val->run())
 			{
-                $mongodb = \Mongo_Db::instance();
-                if ( $insert_id = $mongodb->insert('tasks', array('name' => 'タスク','parent' => 0)))
+                $mongodb = Mongo_Db::instance();
+                if ( $insert_id = $mongodb->insert('tasks',
+                    array(
+                        'name' => $val->validated('name'),
+                        'parent' => $val->validated('parent'),
+                        'finished' => $val->validated('finished'),
+                        'deleted' => $val->validated('deleted')
+                    ))
+                )
                 {
                     Session::set_flash('success', 'Added task #'.$insert_id.'.');
                     Response::redirect('task');
@@ -58,7 +65,8 @@ class Controller_Task extends Controller_Template{
 	{
 		is_null($id) and Response::redirect('task');
 
-		if ( ! $task = Model_Task::find($id))
+        $mongodb = Mongo_Db::instance();
+		if ( ! $mongodb->where(array('id' => $id))->get('tasks'))
 		{
 			Session::set_flash('error', 'Could not find task #'.$id);
 			Response::redirect('task');
@@ -68,28 +76,33 @@ class Controller_Task extends Controller_Template{
 
 		if ($val->run())
 		{
+            $task = $mongodb->get('tasks');
 			$task->name = Input::post('name');
 			$task->parent = Input::post('parent');
 			$task->finished = Input::post('finished');
 			$task->deleted = Input::post('deleted');
 
-			if ($task->save())
-			{
+			if ($mongodb->update('tasks', array(
+                'name' => $task->name,
+                'parent' => $task->parent,
+                'finished' => $task->finished,
+                'deleted' => $task->deleted
+            )))
+            {
 				Session::set_flash('success', 'Updated task #' . $id);
 
 				Response::redirect('task');
 			}
-
-			else
-			{
+            else
+            {
 				Session::set_flash('error', 'Could not update task #' . $id);
 			}
 		}
-
 		else
 		{
 			if (Input::method() == 'POST')
 			{
+                $task = Model_Task::forge();
 				$task->name = $val->validated('name');
 				$task->parent = $val->validated('parent');
 				$task->finished = $val->validated('finished');
@@ -110,13 +123,12 @@ class Controller_Task extends Controller_Template{
 	{
 		is_null($id) and Response::redirect('task');
 
-		if ($task = Model_Task::find($id))
+        $mongodb = Mongo_Db::instance();
+		if ($mongodb->where(array('id' => $id))->get('tasks'))
 		{
-			$task->delete();
-
+			$mongodb->delete('users');
 			Session::set_flash('success', 'Deleted task #'.$id);
 		}
-
 		else
 		{
 			Session::set_flash('error', 'Could not delete task #'.$id);
