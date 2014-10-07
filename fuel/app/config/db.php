30e206a2ccbec64b0b0c8e65378bf424e20a3996
gurimmer <s.reianai@gmail.com>
2014-10-07 16:24:36 +0900

    直接configでパースして設定してみる

--- fuel/app/config/db.php
+++ fuel/app/config/db.php
@@ -1,17 +1,29 @@
 <?php
+
+if (array_key_exists('HEROKU_POSTGRESQL_BLACK_URL', $_SERVER)) {
+    $dbConfigPattern = '/postgres:\/\/(?:([^:^@]+)(?::([^@]+))?@)?([^:^\/]+)(?::(\d+))?\/(.+)/';
+    if (preg_match($dbConfigPattern, $_SERVER["HEROKU_POSTGRESQL_BLACK_URL"], $matches)) {
+        list($dbConfig, $dbuser, $dbpass, $dbhost, $dbport, $dbname) = $matches;
+        return array(
+            'default' => array(
+                'type' => 'pdo',
+                'connection'  => array(
+                    'dsn'        => 'pgsql:host='.$dbhost.';dbname='.$dbname,
+                    'username'   => $dbuser,
+                    'password'   => $dbpass,
+                ),
+            ),
+        );
+    }
+}
+
 return array(
-	'development' => 
-	array(
-		'default' => 
-		array(
-			'type' => 'pdo',
-			'table_prefix' => '',
-			'connection' => 
-			array(
-				'dsn' => 'mysql:host=localhost;dbname=fuel_task',
-				'username' => 'root',
-				'password' => 'root',
-			),
-		),
-	),
+    'default' => array(
+        'type' => 'pdo',
+        'connection'  => array(
+            'dsn'        => 'mysql:host=localhost;dbname=fuel_task',
+            'username'   => 'root',
+            'password'   => 'root',
+        ),
+    ),
 );
